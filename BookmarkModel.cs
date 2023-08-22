901ed05b7911ff70e7a0e56175d5a593a3548f56
Tom Serrante <tserrante@gmail.com>
2023-08-21 23:28:05 -0400

    first commit

---
+++ BookmarkModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BookmarkReader.Model;
+public abstract class BookmarkElement
+{
+    public BookmarkElement() {}
+    [JsonPropertyName("date_added")]
+    [JsonPropertyOrder(-7)]
+    public string DateAdded {get; set;}
+    [JsonPropertyName("date_last_used")]
+    [JsonPropertyOrder(-6)]
+    public string DateLastUsed {get; set;}
+    [JsonPropertyName("guid")]
+    [JsonPropertyOrder(-4)]
+    public string Guid {get; set;}
+    [JsonPropertyName("id")]
+    [JsonPropertyOrder(-3)]
+    public string Id {get; set;}
+    [JsonPropertyName("name")]
+    [JsonPropertyOrder(-2)]
+    public string Name {get; set;}
+    [JsonPropertyName("type")]
+    [JsonPropertyOrder(-1)]
+    public string Type {get; set;}
+    public virtual string Url {get; set;}
+    public virtual string DateModified {get; set;}
+    public virtual List<BookmarkElement> Children{get; set;}
+}
+
+public class Bookmark : BookmarkElement
+{
+    public Bookmark() { }
+    [JsonPropertyName("url")]
+    public override string Url {get; set;} 
+}
+
+public class Folder : BookmarkElement
+{
+    public Folder() {}
+    [JsonPropertyName("date_modified")]
+    [JsonPropertyOrder(-5)]
+    public override string DateModified {get; set;}
+    [JsonPropertyName("children")]
+    [JsonPropertyOrder(-8)]
+    public override List<BookmarkElement> Children {get; set;}
+}
+
+// public class Root : BookmarkElement
+// {
+//     public Root() {}
+//     [JsonPropertyName("date_modified")]
+//     public string DateModified {get; set;}
+//     [JsonPropertyName("children")]
+//     public List<BookmarkElement> RootFolder {get; set;}
+// }
+
+public class BookmarkModel
+{
+    public BookmarkModel() {}
+    [JsonPropertyName("checksum")]
+    public string Checksum {get; set;}
+    [JsonPropertyName("roots")]
+    public Dictionary<string, Folder> Roots {get; set;}
+    [JsonPropertyName("version")]
+    public int Version {get; set;}
+}
+
+public class BookmarkElementConverter : JsonConverter<BookmarkElement>
+{
+    public override BookmarkElement? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if(reader.TokenType != JsonTokenType.StartObject) 
+			throw new JsonException();
+
+        using var jsonDocument = JsonDocument.ParseValue(ref reader);
+        if (!jsonDocument.RootElement.TryGetProperty("type", out var typeProperty))
+            throw new JsonException();
+        return typeProperty.GetString() switch
+        {
+            "url" => jsonDocument.RootElement.Deserialize<Bookmark>(options),
+            "folder" => jsonDocument.RootElement.Deserialize<Folder>(options),
+            _ => throw new JsonException(),
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, BookmarkElement value, JsonSerializerOptions options) =>
+		JsonSerializer.Serialize(writer, value, value.GetType(), options);
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using BookmarkReader.Model;
+using System.IO;
+using System.Text.Json;
+
+namespace BookmarkReader;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonInputFile = File.ReadAllText("Bookmarks");
+        var options = new JsonSerializerOptions
+        {
+            Converters = { new BookmarkElementConverter() },
+            PropertyNameCaseInsensitive = true, // If you want case-insensitive property name matching
+            WriteIndented = true
+        };
+        BookmarkModel bookmarkModel = JsonSerializer.Deserialize<BookmarkModel>(jsonInputFile, options);
+        string jsonOutputString = JsonSerializer.Serialize(bookmarkModel, options);
+        
+        foreach(var rootElem in bookmarkModel.Roots.Values)
+        {
+            Console.WriteLine("Name: {0}\tType: {1}",rootElem.Name, rootElem.Type);
+
+            foreach(var childElem in rootElem.Children)
+            {
+                Console.Write("\t\tName: {0}\tType: {1}", childElem.Name, childElem.Type);
+                if(childElem.GetType() == typeof(Folder))
+                {
+                    foreach(var folderElem in childElem.Children)
+                    {
+                        Console.Write("\t\t\tName: {0}\tType: {1}", folderElem.Name, folderElem.Type);
+                        if(childElem.GetType() == typeof(Bookmark))
+                        {
+                            Console.Write("\tURL: {0}", folderElem.Url);
+                        }          
+                    }
+                    if(childElem.GetType() == typeof(Bookmark))
+                    {
+                        Console.Write("\tURL: {0}", childElem.Url);
+                    }                
+                }
+
+                if(childElem.GetType() == typeof(Bookmark))
+                {
+                    Console.Write("\tURL: {0}", childElem.Url);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        // try
+        // {
+        //     TestClass.Test();
+        // }
+        // catch (Exception ex)
+        // {
+        //     Console.WriteLine("Failed with unhandled exception: ");
+        //     Console.WriteLine(ex);
+        //     throw;
+        // }
+        //File.WriteAllText("reserialized-output.json", jsonOutputString);
+
+        Console.WriteLine("Environment version: {0} ({1}), {2}", System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription , Environment.Version, Environment.OSVersion);
+        Console.WriteLine("System.Text.Json version: " + typeof(JsonSerializer).Assembly.FullName);
+        Console.WriteLine();
+    }
+}
+
+
+// class TestClass
+// {
+//     public static void Test()
+//     {
+//         //var json = GetJson();
+//         var json = File.ReadAllText("Bookmarks");
+//         var options = new JsonSerializerOptions
+//         {
+//             Converters = { new BookmarkElementConverter() },
+//             // Add any additional required options here:
+//             WriteIndented = true,
+//         };
+        
+//         var model = JsonSerializer.Deserialize<BookmarkModel>(json, options);
+
+//         var json2 = JsonSerializer.Serialize(model, options);
+        
+//         Console.WriteLine("Re-serialized {0}", model);
+        
+//         Console.WriteLine(json2);
+//     }
+// }
